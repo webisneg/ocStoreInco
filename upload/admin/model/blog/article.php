597eba55dacad8bebdf12d9a7171337505a6bcc1
dinox-pro <rebrovoleksandr@gmail.com>
2021-07-05 16:37:26 +0300

    Fix https://opencartforum.com/topic/135851-oshibka-pri-sozdanii-stati-kategorii-stati-vse-chto-svyazano-s-blogom/

--- upload/admin/model/blog/article.php
+++ upload/admin/model/blog/article.php
@@ -13,7 +13,7 @@ class ModelBlogArticle extends Model {
 		}
 
 		foreach ($data['article_description'] as $language_id => $value) {
-			$this->db->query("INSERT INTO " . DB_PREFIX . "article_description SET article_id = '" . (int)$article_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "', description = '" . $this->db->escape($value['description']) . "', tag = '" . $this->db->escape($value['tag']) . "', meta_title = '" . $this->db->escape($value['meta_title']) . "', meta_h1 = '" . $this->db->escape($value['meta_h1']) . "', meta_description = '" . $this->db->escape($value['meta_description']) . "', meta_keyword = '" . $this->db->escape($value['meta_keyword']) . "'");
+			$this->db->query("INSERT INTO " . DB_PREFIX . "article_description SET article_id = '" . (int)$article_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "', description = '" . $this->db->escape($value['description']) . "', meta_title = '" . $this->db->escape($value['meta_title']) . "', meta_h1 = '" . $this->db->escape($value['meta_h1']) . "', meta_description = '" . $this->db->escape($value['meta_description']) . "', meta_keyword = '" . $this->db->escape($value['meta_keyword']) . "'");
 		}
 
 		if (isset($data['article_store'])) {
@@ -320,8 +320,7 @@ class ModelBlogArticle extends Model {
 				'meta_title'       => $result['meta_title'],
 				'meta_h1'	       => $result['meta_h1'],
 				'meta_description' => $result['meta_description'],
-				'meta_keyword'     => $result['meta_keyword'],
-				'tag'              => $result['tag']
+				'meta_keyword'     => $result['meta_keyword']
 			);
 		}
 
