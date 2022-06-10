cb007529ef51f33799af654910f14d78f13202b6
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2022-06-10 16:02:02 +0200

    manufacturer lang

--- upload/extension/ocStore/admin/language/en-gb/module/manufacturer.php
+++ upload/extension/ocStore/admin/language/en-gb/module/manufacturer.php
@@ -1,58 +1,17 @@
 <?php
-namespace Opencart\Admin\Controller\Extension\Opencart\Module;
-class Category extends \Opencart\System\Engine\Controller {
-	public function index(): void {
-		$this->load->language('extension/opencart/module/category');
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
 
-		$this->document->setTitle($this->language->get('heading_title'));
+// Heading
+$_['heading_title']    = 'Manufacturer';
 
-		$data['breadcrumbs'] = [];
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified manufacturer module!';
+$_['text_edit']        = 'Edit Manufacturer Module';
 
-		$data['breadcrumbs'][] = [
-			'text' => $this->language->get('text_home'),
-			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'])
-		];
+// Entry
+$_['entry_status']     = 'Status';
 
-		$data['breadcrumbs'][] = [
-			'text' => $this->language->get('text_extension'),
-			'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module')
-		];
-
-		$data['breadcrumbs'][] = [
-			'text' => $this->language->get('heading_title'),
-			'href' => $this->url->link('extension/opencart/module/category', 'user_token=' . $this->session->data['user_token'])
-		];
-
-		$data['save'] = $this->url->link('extension/opencart/module/category|save', 'user_token=' . $this->session->data['user_token']);
-		$data['back'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module');
-
-		$data['module_category_status'] = $this->config->get('module_category_status');
-
-		$data['header'] = $this->load->controller('common/header');
-		$data['column_left'] = $this->load->controller('common/column_left');
-		$data['footer'] = $this->load->controller('common/footer');
-
-		$this->response->setOutput($this->load->view('extension/opencart/module/category', $data));
-	}
-
-	public function save(): void {
-		$this->load->language('extension/opencart/module/category');
-
-		$json = [];
-
-		if (!$this->user->hasPermission('modify', 'extension/opencart/module/category')) {
-			$json['error']['warning'] = $this->language->get('error_permission');
-		}
-
-		if (!$json) {
-			$this->load->model('setting/setting');
-
-			$this->model_setting_setting->editSetting('module_category', $this->request->post);
-
-			$json['success'] = $this->language->get('text_success');
-		}
-
-		$this->response->addHeader('Content-Type: application/json');
-		$this->response->setOutput(json_encode($json));
-	}
-}
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify manufacturer module!';
